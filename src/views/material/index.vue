a9832afdb18a4a0cdd45428c1711d529c78ffbf7
zxl <861555291@qq.com>
2019-12-23 20:47:48 +0800

    素材列表的页面结构

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -4,13 +4,32 @@
       <bread-crumb slot="header">
       <template slot="title">素材管理</template>
       <!-- slot指向title -->
-
       </bread-crumb>
+      <!-- 素材 -->
+      <el-tabs v-model="activeName">
+          <el-tab-pane label="全部素材" name="all">
+              <!-- 全部素材内容 -->
+
+          </el-tab-pane>
+          <el-tab-pane label="收藏素材" name="collect"></el-tab-pane>
+      </el-tabs>
+      <!-- <button @click="test">测试</button> -->
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'collect'
+    }
+  },
+  methods: {
+    // test () {
+    //   this.activeName = 'all'
+    // }
+
+  }
 
 }
 </script>
