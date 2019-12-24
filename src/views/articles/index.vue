aa498d6037b7bdc0d18580706fee82f24d880e62
zxl <861555291@qq.com>
2019-12-24 22:51:55 +0800

    内容列表-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -36,9 +36,8 @@
       </el-col>
       <el-col :span="18">
         <el-date-picker
-         @change="changeCondition"
-         value-format="yyyy-MM-dd"
-
+          @change="changeCondition"
+          value-format="yyyy-MM-dd"
           v-model="formData.dateRange"
           type="daterange"
           range-separator="-"
@@ -49,7 +48,7 @@
     </el-row>
     <!-- 主体部分 -->
     <el-row class="total">
-      <span>共找到100条符合条件的内容</span>
+      <span>共找到{{page.total}}条符合条件的内容</span>
     </el-row>
     <!-- 循环模板 -->
     <el-row
@@ -85,6 +84,16 @@
         </el-row>
       </el-col>
     </el-row>
+    <el-row type="flex" justify="center" align="middle" style="height:60px">
+      <el-pagination
+      background
+      :page-size="page.pageSize"
+      :current-page="page.currentPage"
+      layout="prev, pager, next"
+      :total="page.total"
+      @current-change="changePage"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -99,7 +108,12 @@ export default {
       },
       channels: [], // 定义一个channels接收频道
       list: [],
-      defaultImg: require('../../assets/img/jie.jpg')
+      defaultImg: require('../../assets/img/jie.jpg'),
+      page: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   filters: {
@@ -134,12 +148,25 @@ export default {
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      // this.getConditionArticle()
+    },
     changeCondition () {
+      this.page.currentPage = 1
+      this.getConditionArticle()
+    },
+    getConditionArticle () {
       let params = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize,
         status: this.formData.status === 5 ? null : this.formData.status,
         channel_id: this.formData.channel_id,
-        begin_pubdate: this.formData.dateRange.length ? this.formData.dateRange[0] : null,
-        end_pubdate: this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null
+        begin_pubdate: this.formData.dateRange.length
+          ? this.formData.dateRange[0]
+          : null,
+        end_pubdate:
+          this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null
       }
       this.getArticles(params)
     },
@@ -157,14 +184,14 @@ export default {
         params
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count // 文章总数
       })
     }
-
   },
   created () {
     this.getChannels() // 调用获取频道数据
 
-    this.getArticles() // 调用获取文章列表
+    this.getArticles({ page: 1, per_page: 10 }) // 调用获取文章列表
   }
 }
 </script>
