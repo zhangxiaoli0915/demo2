50319d25dc20ea445cf478f9602c1130f0de4fa5
zxl <861555291@qq.com>
2019-12-28 20:57:53 +0800

    页面进度条引用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,7 +1,10 @@
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 全局前置守卫
 router.beforeEach(function (to, from, next) {
-// 拦截谁 判断拦截地址 如果是登录页的话要令牌，如哦有的话就放过，否则强制跳转到登录页
+  progress.start()// 开启进度条
+  // 拦截谁 判断拦截地址 如果是登录页的话要令牌，如哦有的话就放过，否则强制跳转到登录页
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -12,4 +15,8 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+// afterEach关闭
+router.afterEach(function () {
+  progress.done()// 路由变化完成执行
 })
